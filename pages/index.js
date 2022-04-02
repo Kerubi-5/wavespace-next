adbcf90ba5ee52c3bc9320c6b4234b320c4b8399
John Kim A. Querobines <querobinesj@gmail.com>
2022-04-02 19:51:46 +0800

    style: adds icons

--- pages/index.js
+++ pages/index.js
@@ -109,6 +109,17 @@ export default function Home() {
     }
   };
 
+  const RenderCard = ({ children, item }) => {
+    return (
+      <>
+        <div className="flex items-center gap-2 divide-solid divide-x-2">
+          <span>{children}</span>
+          <p className="px-2">{item}</p>
+        </div>
+      </>
+    );
+  };
+
   useEffect(() => {
     checkIfWalletIsConnected();
   }, []);
@@ -292,14 +303,59 @@ export default function Home() {
           console.log(item.time);
           return (
             <div
-              className="bg-gray-50 shadow-sm p-4 rounded-md break-all"
+              className="bg-gray-50 shadow-sm p-4 rounded-md break-all grid gap-4 md:grid-cols-2"
               key={item.time}
             >
-              <div>Address: {item.address}</div>
-              <div>
-                Time: {item.time.toLocaleString("en-GB", { timeZone: "UTC" })}
-              </div>
-              <div>Message: {item.msg}</div>
+              <RenderCard item={item.address}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+                  />
+                </svg>
+              </RenderCard>
+              <RenderCard
+                item={item.time.toLocaleString("en-GB", { timeZone: "UTC" })}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+              </RenderCard>
+              <RenderCard item={item.msg}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z"
+                  />
+                </svg>
+              </RenderCard>
             </div>
           );
         })}

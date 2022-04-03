6af0e6d05a22bd130c3eb6aea7675298bd1d11cd
John Kim A. Querobines <querobinesj@gmail.com>
2022-04-03 18:34:49 +0800

    feat: adds user in navbar on connect

--- components/Menu.js
+++ components/Menu.js
@@ -4,7 +4,25 @@ const Menu = ({ connectWallet, currentAccount }) => {
       <div>
         <h1 className="text-xl font-extrabold">KK</h1>
       </div>
-      {!currentAccount && (
+      {currentAccount ? (
+        <div className="flex items-center gap-2 bg-gray-500 p-2 px-4 rounded-md text-white w-24 md:w-auto">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+            />
+          </svg>
+          <span className="truncate">{currentAccount}</span>
+        </div>
+      ) : (
         <div className="flex items-center gap-2 bg-gray-500 p-2 rounded-md text-white">
           <svg
             xmlns="http://www.w3.org/2000/svg"
